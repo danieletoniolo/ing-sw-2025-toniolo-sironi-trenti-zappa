97293e923f7517a4ea13e72869186dcb4e49721f
danieletoniolo <dt.danieletoniolo@gmail.com>
2025-03-04 14:02:34 +0100

    Added Good and GoodType

---
+++ src/main/java/Good/Good.java
@@ -0,0 +1,17 @@
+package Good;
+
+public class Good {
+    private GoodType color;
+
+    Good(GoodType color) {
+        this.color = color;
+    }
+
+    public GoodType getColor() {
+        return color;
+    }
+
+    public int getValue() {
+        return color.getValue();
+    }
+}
--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens org.example.demo to javafx.fxml;
-    exports org.example.demo;
-}
