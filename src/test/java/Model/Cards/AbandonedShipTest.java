222b6204865ae814fbde6f5e0f8f87f663cadba4
Mz2305 <matteo.zappa23@gmail.com>
2025-03-07 16:08:40 +0100

    Creazione AbandonedShipTest -- Non testato

---
+++ src/test/java/Model/Cards/AbandonedShipTest.java
@@ -0,0 +1,44 @@
+package Model.Cards;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbandonedShipTest {
+    AbandonedShip card;
+    //PlayerData player;
+
+    @BeforeEach
+    void setUp() {
+        card = new AbandonedShip(2,3,1,2);
+        assertNotNull(card, "Variabile card non inizializzata correttamente");
+
+        //player = new PlayerData();
+        //assertNotNull(player, "Variabile player non inizializzata correttamente");
+    }
+
+    @Test
+    void getCrewRequired() {
+        assertEquals(3,card.getCrewRequired());
+    }
+
+    @Test
+    void getFlightDays() {
+        assertEquals(1,card.getFlightDays());
+    }
+
+    @Test
+    void getCredit() {
+        assertEquals(2,card.getCredit());
+    }
+
+    @Test
+    void getCardType() {
+        assertEquals(CardType.ABANDONEDSHIP, card.getCardType());
+    }
+
+    @Test
+    void apply() {
+    }
+}
