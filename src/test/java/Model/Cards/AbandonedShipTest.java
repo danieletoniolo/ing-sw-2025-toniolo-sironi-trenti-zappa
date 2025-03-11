b00766b392afd219e137a40b32b19609d8c9957a
Mz2305 <matteo.zappa23@gmail.com>
2025-03-11 16:51:32 +0100

    Test AbandonedShipTest randomized and generalized

--- src/test/java/Model/Cards/AbandonedShipTest.java
+++ src/test/java/Model/Cards/AbandonedShipTest.java
@@ -4,18 +4,24 @@ import Model.Player.PlayerColor;
 import Model.Player.PlayerData;
 import Model.SpaceShip.SpaceShip;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AbandonedShipTest {
     AbandonedShip card;
     PlayerData player;
+    Random random = new Random();
 
     @BeforeEach
     void setUp() {
-        card = new AbandonedShip(2,3,1,2);
-        assertNotNull(card, "Variabile card non inizializzata correttamente");
+        card = new AbandonedShip(2,3,3,2);
+        assertNotNull(card);
     }
 
     @Test
@@ -30,7 +36,7 @@ class AbandonedShipTest {
 
     @Test
     void getFlightDays() {
-        assertEquals(1,card.getFlightDays());
+        assertEquals(3,card.getFlightDays());
     }
 
     @Test
@@ -58,5 +64,35 @@ class AbandonedShipTest {
         assertNotNull(player);
         card.apply(player);
         assertTrue(card.isPlayed());
+    }
+
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, 2, 3, 50",
+            "2, 3, 5, 100",
+            "3, 4, 6, 150"
+    })
+    void testCardInitialization(int level, int crew, int flightDays, int credit) {
+        AbandonedShip testCard = new AbandonedShip(level, crew, flightDays, credit);
+        assertEquals(level, testCard.getCardLevel());
+        assertEquals(crew, testCard.getCrewRequired());
+        assertEquals(flightDays, testCard.getFlightDays());
+        assertEquals(credit, testCard.getCredit());
+    }
+
+    @RepeatedTest(5)
+    void testRandomizedInitialization() {
+        // + 1 because the highest integer number doesn't belong to the interval of the randomized
+        int crew = random.nextInt(card.getCrewRequired()) + 1;
+        int flightDays = random.nextInt(card.getFlightDays()) + 1;
+        int credit = random.nextInt(card.getCredit()) + 1;
+
+        AbandonedShip randomCard = new AbandonedShip(2, crew, flightDays, credit);
+        System.out.println(randomCard.getCrewRequired() + " " + randomCard.getFlightDays() + " " + randomCard.getCredit());
+        assertEquals(crew, randomCard.getCrewRequired());
+        assertEquals(flightDays, randomCard.getFlightDays());
+        assertEquals(credit, randomCard.getCredit());
     }
 }
