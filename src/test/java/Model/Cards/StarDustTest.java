307b71b87be6fcf42bcf97edabab3c0a82e7a513
Mz2305 <matteo.zappa23@gmail.com>
2025-03-23 18:39:39 +0100

    Fixed some tests in StarDustTest

--- src/test/java/Model/Cards/StarDustTest.java
+++ src/test/java/Model/Cards/StarDustTest.java
@@ -13,30 +13,22 @@ class StarDustTest {
 
     @BeforeEach
     void setUp() {
-        card = new StarDust(2);
+        card = new StarDust(2, 0);
         assertNotNull(card);
     }
 
-    @Test
+    @RepeatedTest(5)
     void getCardLevel() {
         assertEquals(2,card.getCardLevel());
+
+        Random rand = new Random();
+        int level = rand.nextInt(3) + 1;
+        StarDust randomCard = new StarDust(level, 0);
+        assertEquals(level, randomCard.getCardLevel());
     }
 
     @Test
     void getCardType() {
         assertEquals(CardType.STARDUST, card.getCardType());
-    }
-
-    @Test
-    void apply() {
-
-    }
-
-    @RepeatedTest(5)
-    void testRandomizedLevelInitialization() {
-        Random random = new Random();
-        int level = random.nextInt(card.getCardLevel()) + 1;
-        card = new StarDust(level);
-        assertEquals(level, card.getCardLevel());
     }
 }
