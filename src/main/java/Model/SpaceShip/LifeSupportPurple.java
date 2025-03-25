ed7c35738689577a6eaf00d1963f2d49d63ce9fb
danieletoniolo <dt.danieletoniolo@gmail.com>
2025-03-25 15:55:33 +0100

    Refactor Cabin and LifeSupport classes: update isValid method

--- src/main/java/Model/SpaceShip/LifeSupportPurple.java
+++ src/main/java/Model/SpaceShip/LifeSupportPurple.java
@@ -5,23 +5,6 @@ public class LifeSupportPurple extends Component {
         super(ID, connectors);
     }
 
-    /**
-     * @implNote Extends the isValid method of the Component class to add purple life support to the cabin next to the component
-     */
-    @Override
-    public boolean isValid() {
-        if (!super.isValid()) {
-            return false;
-        }
-        for (Component component : ship.getSurroundingComponents(row, column)) {
-            if (component != null && component.getComponentType() == ComponentType.CABIN) {
-                Cabin cabin = (Cabin) component;
-                cabin.addPurpleLifeSupport();
-            }
-        }
-        return true;
-    }
-
     @Override
     public ComponentType getComponentType() {
         return ComponentType.PURPLE_LIFE_SUPPORT;
