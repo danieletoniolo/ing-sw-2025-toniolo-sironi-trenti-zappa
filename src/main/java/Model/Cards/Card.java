5687cd45909f7206ff64bb6ea4c02bee8ed8d02e
Lorenzo Trenti <lorenzo.trenti103@gmail.com>
2025-03-25 13:26:14 +0100

    Enhance Card serialization with Jackson annotations and fix typos in Cards.json

--- src/main/java/Model/Cards/Card.java
+++ src/main/java/Model/Cards/Card.java
@@ -1,5 +1,22 @@
 package Model.Cards;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+// 📌 Permette a Jackson di capire quale sottoclasse usare
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = Slavers.class, name = "Slavers"),
+        @JsonSubTypes.Type(value = Smugglers.class, name = "Smugglers"),
+        @JsonSubTypes.Type(value = Pirates.class, name = "Pirates"),
+        @JsonSubTypes.Type(value = StarDust.class, name = "StarDust"),
+        @JsonSubTypes.Type(value = OpenSpace.class, name = "OpenSpace"),
+        @JsonSubTypes.Type(value = MeteorSwarm.class, name = "MeteorSwarm"),
+        @JsonSubTypes.Type(value = Planets.class, name = "Planets"),
+        @JsonSubTypes.Type(value = CombatZone.class, name = "CombatZone"),
+        @JsonSubTypes.Type(value = AbandonedShip.class, name = "AbandonedShip"),
+        @JsonSubTypes.Type(value = AbandonedStation.class, name = "AbandonedStation")
+})
 public abstract class Card {
     private final int level;
     private final int ID;
