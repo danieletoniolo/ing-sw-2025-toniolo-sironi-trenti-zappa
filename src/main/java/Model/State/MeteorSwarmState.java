dcad3c34460f2e6533efff3bbd42ce30c5a25e4d
Vittorio Sironi <vittorio.sironi@icloud.com>
2025-03-29 18:29:26 +0100

    Refactor CombatZoneState and MeteorSwarmState: simplify state management by introducing FightHandler; remove unused variables and methods. Created FightHandler

--- src/main/java/Model/State/MeteorSwarmState.java
+++ src/main/java/Model/State/MeteorSwarmState.java
@@ -1,25 +1,16 @@
 package Model.State;
 
-import Model.Cards.Hits.Hit;
 import Model.Cards.MeteorSwarm;
 import Model.Player.PlayerData;
-import Model.SpaceShip.Component;
-import Model.SpaceShip.SpaceShip;
+import Model.State.Handler.FightHandler;
+import Model.State.Interface.Fightable;
 import org.javatuples.Pair;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MeteorSwarmState extends State {
+public class MeteorSwarmState extends State implements Fightable {
     private final MeteorSwarm card;
-    private Integer dice;
-    private int hitIndex;
-    private int fightState;
-    private Boolean protect;
-    private List<List<Pair<Integer, Integer>>> fragments;
-    private Pair<Component, Integer> protectionResult;
-    private Integer fragmentChoice;
-    private Integer batteryID;
-
+    private final FightHandler fightHandler;
 
     /**
      * Constructor
@@ -29,73 +20,7 @@ public class MeteorSwarmState extends State {
     public MeteorSwarmState(ArrayList<PlayerData> players, MeteorSwarm card) {
         super(players);
         this.card = card;
-        this.dice = null;
-        this.fragmentChoice = null;
-        this.protect = null;
-        this.batteryID = null;
-        this.hitIndex = -1;
-        this.fightState = 0;
-    }
-
-    /**
-     * Transition to the next hit
-     */
-    private void transitionHit() {
-        hitIndex++;
-        fightState = 0;
-    }
-
-    private void executeSubStateProtection(SpaceShip spaceShip) {
-        Component component =  protectionResult.getValue0();
-        int protectionType = protectionResult.getValue1();
-
-        if (protectionType == 0 || protectionType == -1) {
-            if (protect) {
-                spaceShip.useEnergy(batteryID);
-                transitionHit();
-            } else {
-                spaceShip.destroyComponent(component.getRow(), component.getColumn());
-                fragments = spaceShip.getDisconnectedComponents();
-                if (fragments.size() > 1) {
-                    fightState++;
-                } else {
-                    transitionHit();
-                }
-            }
-        }
-    }
-
-    private void executeSubStateFight(PlayerData player) throws IllegalStateException {
-        SpaceShip spaceShip = player.getSpaceShip();
-
-        switch (fightState) {
-            case 0:
-                if (hitIndex >= card.getMeteors().size()) {
-                    throw new IndexOutOfBoundsException("Hit index out of bounds");
-                }
-                if (dice == null) {
-                    throw new IllegalStateException("Dice not set");
-                }
-                Hit hit = card.getMeteors().get(hitIndex);
-                protectionResult = spaceShip.canProtect(dice, hit);
-                fightState++;
-                break;
-            case 1:
-                if (protect == null) {
-                    throw new IllegalStateException("Protect not set");
-                }
-                executeSubStateProtection(spaceShip);
-                break;
-            case 2:
-                if (fragmentChoice == null) {
-                    throw new IllegalStateException("FragmentChoice not set");
-                }
-                for (Pair<Integer, Integer> fragment : fragments.get(fragmentChoice)) {
-                    spaceShip.destroyComponent(fragment.getValue0(), fragment.getValue1());
-                }
-                transitionHit();
-                break;
-        }
+        this.fightHandler = new FightHandler();
     }
 
     /**
@@ -104,10 +29,10 @@ public class MeteorSwarmState extends State {
      * @throws IllegalStateException if method called is done in the wrong order
      */
     public List<List<Pair<Integer, Integer>>> getFragments() throws IllegalStateException {
-        if (fightState != 3) {
-            throw new IllegalStateException("getDisconnectedComponents done in the wrong order, current state: 3");
+        if (fightHandler.getFightState() != 2) {
+            throw new IllegalStateException("getDisconnectedComponents done in the wrong order, current state: " + fightHandler.getFightState());
         }
-        return fragments;
+        return fightHandler.getFragments();
     }
 
     /**
@@ -116,10 +41,7 @@ public class MeteorSwarmState extends State {
      * @throws IllegalStateException if not in the right state in order to do the action
      */
     public void setFragmentChoice(int fragmentChoice) throws IllegalStateException {
-        if (fightState != 2) {
-            throw new IllegalStateException("Fragment choice not allowed in this state");
-        }
-        this.fragmentChoice = fragmentChoice;
+        fightHandler.setFragmentChoice(fragmentChoice);
     }
 
     /**
@@ -130,14 +52,7 @@ public class MeteorSwarmState extends State {
      * @throws IllegalArgumentException if batteryID_ is null and protect_ is true
      */
     public void setProtect(boolean protect_, Integer batteryID_) throws IllegalStateException, IllegalArgumentException {
-        if (fightState != 1) {
-            throw new IllegalStateException("Battery ID not allowed in this state");
-        }
-        this.protect = protect_;
-        if (protect_ && batteryID_ == null) {
-            throw new IllegalArgumentException("If you set protect to true, you have to set the batteryID");
-        }
-        this.batteryID = batteryID_;
+        fightHandler.setProtect(protect_, batteryID_);
     }
 
     /**
@@ -145,10 +60,7 @@ public class MeteorSwarmState extends State {
      * @param dice dice value
      */
     public void setDice(int dice) {
-        if (fightState != 0) {
-            throw new IllegalStateException("Dice not allowed in this state");
-        }
-        this.dice = dice;
+        fightHandler.setDice(dice);
     }
 
     /**
@@ -167,6 +79,11 @@ public class MeteorSwarmState extends State {
      */
     @Override
     public void execute(PlayerData player) throws IndexOutOfBoundsException, IllegalStateException {
-        this.executeSubStateFight(player);
+        int currentHitIndex = fightHandler.getHitIndex();
+        if (currentHitIndex >= card.getMeteors().size()) {
+            throw new IndexOutOfBoundsException("Hit index out of bounds");
+        }
+
+        fightHandler.executeFight(player.getSpaceShip(), () -> card.getMeteors().get(currentHitIndex));
     }
 }
