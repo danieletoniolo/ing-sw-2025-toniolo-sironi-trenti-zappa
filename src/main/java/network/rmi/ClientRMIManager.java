e042d8295c1c816f5cb78a3714f0e9da0ba38da2
Vittorio Sironi <vittorio.sironi@icloud.com>
2025-04-14 18:28:55 +0200

    Implement RMI support in Client and GameController
    Add methods for user management and registry setup

--- src/main/java/network/rmi/ClientRMIManager.java
+++ src/main/java/network/rmi/ClientRMIManager.java
@@ -7,24 +7,26 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
-public class ManagerClientRMI {
+public class ClientRMIManager {
     private static final int DEFAULT_REGISTRY_PORT = 1099;
     private static String registryIP = null;
     private static Integer registryPort = null;
 
     public static void setRegistryIPPort(String registryIP, Integer registryPort) {
-        ManagerClientRMI.registryIP = registryIP;
-        ManagerClientRMI.registryPort = registryPort;
+        ClientRMIManager.registryIP = registryIP;
+        ClientRMIManager.registryPort = registryPort;
     }
 
     public static void setRegistryIP(String registryIP) {
-        ManagerClientRMI.setRegistryIPPort(registryIP, DEFAULT_REGISTRY_PORT);
+        ClientRMIManager.setRegistryIPPort(registryIP, DEFAULT_REGISTRY_PORT);
     }
 
-    public static IGameController getStub(String remoteObjectName) throws RemoteException, NotBoundException {
-        Registry registry = LocateRegistry.getRegistry();
+    public static IGameController getStub(String remoteObjectName) throws RemoteException, NotBoundException, IllegalStateException {
+        if (registryIP == null && registryPort == null) {
+            throw new IllegalStateException("Registry IP and port have not been set yet");
+        }
 
-        String[] namesBound = registry.list();
+        Registry registry = LocateRegistry.getRegistry();
         return (IGameController) registry.lookup(remoteObjectName);
     }
 }
