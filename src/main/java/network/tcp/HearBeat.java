5120975317ae11a3bd470f82552235e4ffd049c3
danieletoniolo <danieletoniolo@users.noreply.github.com>
2025-04-15 09:23:42 +0200

    Add message classes for network communication: implement heartbeat, single, double, triple argument messages, and message type enumeration

---
+++ src/main/java/network/tcp/HearBeat.java
@@ -0,0 +1,10 @@
+package network.tcp;
+
+import java.io.Serializable;
+
+/**
+ * This class is used to send a heartbeat message over the network.
+ * The client has to send a heartbeat message periodically in order to not be disconnected.
+ */
+public class HearBeat implements Serializable {
+}
