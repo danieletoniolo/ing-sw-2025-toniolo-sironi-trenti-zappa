bf8e0a7fb23823877eb9089963566a5e4a49a575
Lorenzo Trenti <lorenzo.trenti103@gmail.com>
2025-04-15 16:37:58 +0200

    Remove coin validation in addCoins method of PlayerData: allow negative coin values for game mechanics

--- src/main/java/Model/Player/PlayerData.java
+++ src/main/java/Model/Player/PlayerData.java
@@ -135,9 +135,6 @@ public class PlayerData {
      */
     public void addCoins(int coins) {
         this.coins += coins;
-        if (this.coins < 0) {
-            this.coins = 0;
-        }
     }
 
     /**
