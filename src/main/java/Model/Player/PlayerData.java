a8991f9c8691f232f72b95c56bc497fb500bbba9
Lorenzo Trenti <lorenzo.trenti103@gmail.com>
2025-04-15 16:20:56 +0200

    Ensure coins do not drop below zero in PlayerData: add validation in addCoins method

--- src/main/java/Model/Player/PlayerData.java
+++ src/main/java/Model/Player/PlayerData.java
@@ -135,6 +135,9 @@ public class PlayerData {
      */
     public void addCoins(int coins) {
         this.coins += coins;
+        if (this.coins < 0) {
+            this.coins = 0;
+        }
     }
 
     /**
