eeaba39993bda9134102cad4d6c5d58eb2b99813
Lorenzo Trenti <lorenzo.trenti103@gmail.com>
2025-04-15 08:31:58 +0200

    Refactor ComponentMainView and ComponentView: improve tile drawing logic and enhance spaceship visualization

--- src/main/java/view/TUI/tiles/ComponentView.java
+++ src/main/java/view/TUI/tiles/ComponentView.java
@@ -2,131 +2,54 @@ package view.TUI.tiles;
 
 import Model.Good.Good;
 import Model.SpaceShip.*;
+import org.javatuples.Pair;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ComponentView {
-    private final int COLS = 12;
-    private final String covered = "  Covered  ";
-    private final String clean = "           ";
-    private Boolean[] checked;
-    private ArrayList<Component> components;
+    private final String covered = "|  Covered  |";
+    private final String clean = "|           |";
 
-    public void setCompnentsToVisualize(ArrayList<Component> components) {
-        this.components = components;
-        checked = new Boolean[components.size()];
-        Arrays.fill(checked, false);
-    }
-
-    public void setViewedTile(Component tile, Boolean status) {
-        if (!components.contains(tile)) {
-            return;
-        }
-        this.checked[components.indexOf(tile)] = status;
-        //viewSingleTile(tile);
-    }
-
-    public void viewSingleTile(Component tile) {
+    /**
+     * Draw one line with tot tiles. If component == null draw spaces, if Boolean of component is set false draw a covered tile
+     * @param components ArrayList of Pair with the component to print and Boolean that say if the tile is covered
+     */
+    public void drawTilesOnOneLine(ArrayList<Pair<Component, Boolean>> components) {
         for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 7; j++) {
-                switch (i) {
-                    case 0, 6:
-                        switch (j) {
-                            case 0, 6 -> System.out.print(". ");
-                            default -> System.out.print("_ ");
-                        }
-                        break;
-                    case 1:
-                        switch (j) {
-                            case 0, 6 -> System.out.print("|");
-                            case 1 -> System.out.print(firstLine(tile));
-                        }
-                        break;
-                    case 2:
-                        switch (j) {
-                            case 0, 6 -> System.out.print("|");
-                            case 1 -> System.out.print(secondLine(tile));
-                        }
-                        break;
-                    case 3:
-                        switch (j) {
-                            case 0, 6 -> System.out.print("|");
-                            case 1 -> System.out.print(middleLine(tile));
-                        }
-                        break;
-                    case 4:
-                        switch (j) {
-                            case 0, 6 -> System.out.print("|");
-                            case 1 -> System.out.print(fourthLine(tile));
-                        }
-                        break;
-                    case 5:
-                        switch (j) {
-                            case 0, 6 -> System.out.print("|");
-                            case 1 -> System.out.print(fifthLine(tile));
-                        }
-                        break;
-                }
-            }
-            System.out.println();
-        }
-    }
-
-    public void drawOnBoardComponents() {
-        int row;
-        for (row = 0; row < components.size()/COLS; row++) {
-            drawLineTiles(row, COLS);
-        }
-        drawLineTiles(row, components.size() % COLS);
-    }
-
-    private void drawLineTiles(int row, int cols) {
-        for (int i = 0; i < 7; i++) {
-            for (int k = 0; k < cols; k++) {
-                Component tile = components.get(row * COLS + k);
-                Boolean checked = this.checked[row * COLS + k];
+            for (int k = 0; k < components.size(); k++) {
+                Component tile = components.get(k).getValue0();
+                Boolean check = components.get(k).getValue1();
                 for (int j = 0; j < 7; j++) {
                     switch (i) {
                         case 0, 6:
-                            switch (j) {
-                                case 0, 6 -> System.out.print(checked == null ? "  " : ". ");
-                                default -> System.out.print(checked == null ? "  " : "_ ");
+                            if (j == 0) {
+                                System.out.print(". _ _ _ _ _ .");
                             }
                             break;
                         case 1:
-                            switch (j) {
-                                case 0 -> System.out.print(checked == null ? " " : "|");
-                                case 1 -> System.out.print(Boolean.TRUE.equals(checked) ? firstLine(tile) : clean);
-                                case 6 -> System.out.print(checked == null ? "  " : "| ");
+                            if (j == 0) {
+                                System.out.print(tile != null && check ? firstLine(tile) : clean);
                             }
                             break;
                         case 2:
-                            switch (j) {
-                                case 0 -> System.out.print(checked == null ? " " : "|");
-                                case 1 -> System.out.print(Boolean.TRUE.equals(checked) ? secondLine(tile) : clean);
-                                case 6 -> System.out.print(checked == null ? "  " : "| ");
+                            if (j == 0) {
+                                System.out.print(tile != null && check ? secondLine(tile) : clean);
                             }
                             break;
                         case 3:
-                            switch (j) {
-                                case 0 -> System.out.print(checked == null ? " " : "|");
-                                case 1 -> System.out.print(Boolean.TRUE.equals(checked) ? middleLine(tile) : Boolean.FALSE.equals(checked) ? covered : clean);
-                                case 6 -> System.out.print(checked == null ? "  " : "| ");
+                            if (j == 0) {
+                                System.out.print(tile != null ? check ? middleLine(tile) : covered : clean);
                             }
                             break;
                         case 4:
-                            switch (j){
-                                case 0 -> System.out.print(checked == null ? " " : "|");
-                                case 1 -> System.out.print(Boolean.TRUE.equals(checked) ? fourthLine(tile) : clean);
-                                case 6 -> System.out.print(checked == null ? "  " : "| ");
+                            if (j == 0) {
+                                System.out.print(tile != null && check ? fourthLine(tile) : clean);
                             }
                             break;
                         case 5:
-                            switch (j){
-                                case 0 -> System.out.print(checked == null ? " " : "|");
-                                case 1 -> System.out.print(Boolean.TRUE.equals(checked) ? fifthLine(tile) : clean);
-                                case 6 -> System.out.print(checked == null ? "  " : "| ");
+                            if (j == 0) {
+                                System.out.print(tile != null && check ? fifthLine(tile) : clean);
                             }
                             break;
                     }
@@ -138,26 +61,26 @@ public class ComponentView {
 
     private String firstLine(Component c) {
         if (c instanceof Engine && c.getClockwiseRotation() == 2) {
-            return "     " + "v" + "     ";
+            return "|     " + "v" + "     |";
         }
         if (c instanceof Cannon && c.getClockwiseRotation() == 0) {
-            return "     " + "^" + "     ";
+            return "|     " + "^" + "     |";
         }
-        return "     " + getConnectionString(c, 0) + "     ";
+        return "|     " + getConnectionString(c, 0) + "     |";
     }
 
     private String secondLine(Component c) {
         //length = 11 for the String
         return switch (c.getComponentType()) {
-            case SINGLE_ENGINE, DOUBLE_ENGINE -> "  Engine   ";
-            case SINGLE_CANNON, DOUBLE_CANNON -> "  Cannon   ";
-            case BROWN_LIFE_SUPPORT, PURPLE_LIFE_SUPPORT -> "   Life    ";
-            case CABIN -> "   Cabin   ";
-            case SHIELD -> "  Shield   ";
-            case BATTERY -> "  Battery  ";
-            case STORAGE -> "  Storage  ";
-            case CONNECTORS -> "   Conne-  ";
-            default -> "           ";
+            case SINGLE_ENGINE, DOUBLE_ENGINE -> "|  Engine   |";
+            case SINGLE_CANNON, DOUBLE_CANNON -> "|  Cannon   |";
+            case BROWN_LIFE_SUPPORT, PURPLE_LIFE_SUPPORT -> "|   Life    |";
+            case CABIN -> "|   Cabin   |";
+            case SHIELD -> "|  Shield   |";
+            case BATTERY -> "|  Battery  |";
+            case STORAGE -> "|  Storage  |";
+            case CONNECTORS -> "|   Conne-  |";
+            default -> clean;
         };
     }
 
@@ -204,17 +127,17 @@ public class ComponentView {
                 right = ">";
             }
         }
-        return left + middle + right;
+        return "|" + left + middle + right + "|";
     }
 
     private String fourthLine(Component c) {
         // length = 11
         return switch (c.getComponentType()) {
-            case BROWN_LIFE_SUPPORT -> "   Brown   ";
-            case PURPLE_LIFE_SUPPORT -> "  Purple   ";
+            case BROWN_LIFE_SUPPORT -> "|   Brown   |";
+            case PURPLE_LIFE_SUPPORT -> "|  Purple   |";
             case STORAGE -> {
                 if (((Storage) c).getGoods() == null) {
-                    yield "           ";
+                    yield clean;
                 }
                 String line = "  G:";
                 for (Good g : ((Storage) c).getGoods()) {
@@ -223,21 +146,21 @@ public class ComponentView {
                 while(line.length() < 11) {
                     line += " ";
                 }
-                yield line;
+                yield "|" + line + "|";
             }
-            case CABIN -> "  Alien:" + (((Cabin) c).hasPurpleAlien() ? "P  " : ((Cabin) c).hasBrownAlien() ? "B  " : "X  ");
-            default -> "           ";
+            case CABIN -> "|  Alien:" + (((Cabin) c).hasPurpleAlien() ? "P  " : ((Cabin) c).hasBrownAlien() ? "B  " : "X  " + "|");
+            default -> clean;
         };
     }
 
     private String fifthLine(Component c) {
         if (c instanceof Engine && c.getClockwiseRotation() == 0) {
-            return "     " + "^" + "     ";
+            return "|     " + "^" + "     |";
         }
         if (c instanceof Cannon && c.getClockwiseRotation() == 2) {
-            return "     " + "v" + "     ";
+            return "|     " + "v" + "     |";
         }
-        return "     " + getConnectionString(c, 2) + "     ";
+        return "|     " + getConnectionString(c, 2) + "     |";
     }
 
     private String getConnectionString(Component c, int face) {
