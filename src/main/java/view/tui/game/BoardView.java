99468142b168285d3d5a7f69e1358dc244b330c1
Lorenzo Trenti <lorenzo.trenti@mail.polimi.it>
2025-05-04 14:49:29 +0200

    Add BoardCommands enum and refactor drawBoard method for improved readability

--- src/main/java/view/tui/game/BoardView.java
+++ src/main/java/view/tui/game/BoardView.java
@@ -5,6 +5,19 @@ import Model.Game.Board.Level;
 import Model.Player.PlayerColor;
 import Model.Player.PlayerData;
 
+enum BoardCommands {
+    VIEW,
+    HELP;
+
+    public static BoardCommands from(String name) {
+        try {
+            return BoardCommands.valueOf(name.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
+}
+
 public class BoardView {
     public void drawBoard(Board board) {
         int rows = (board.getStepsForALap()/4)*3 - 2;
@@ -25,114 +38,110 @@ public class BoardView {
 
         board.addSteps(player4, 30);
 
-        for (int i = 0; i < board.getStepsForALap(); i++) {
-            switch (board.getBoardLevel()) {
-                case LEARNING:
-                    switch (i){
-                        case 0 -> steps[i] = "4";
-                        case 1 -> steps[i] = "3";
-                        case 2 -> steps[i] = "2";
-                        case 4 -> steps[i] = "1";
-                        default -> steps[i] = " ";
-                    }
-                    break;
-                case SECOND:
-                    switch (i){
-                        case 0 -> steps[i] = "4";
-                        case 1 -> steps[i] = "3";
-                        case 3 -> steps[i] = "2";
-                        case 6 -> steps[i] = "1";
-                        default -> steps[i] = " ";
-                    }
-                    break;
-            }
-        }
-        board.refreshInGamePlayers();
-        board.getInGamePlayers().forEach(player -> {
-            steps[player.getStep() % board.getStepsForALap()] = getPlayerColor(player);
-        });
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                switch (i % 3) {
-                    case 0:
-                        if (i == 0) {
-                            switch (j % 7) {
-                                case 0:
-                                    System.out.print("[ " + steps[up] + " ]");
-                                    up++;
-                                    break;
-                                case 5:
-                                    System.out.print("->");
-                                    break;
-                            }
-                        }
-                        else if (i == rows - 1) {
-                            switch (j % 7) {
-                                case 0:
-                                    System.out.print("[ " + steps[down] + " ]");
-                                    down--;
-                                    break;
-                                case 5:
-                                    System.out.print("<-");
-                                    break;
-                            }
-                        }
-                        else {
-                            if (j == cols - 5) {
-                                System.out.print("[ " + steps[up] + " ]");
-                                up++;
-                            }
-                            else if (j == 0) {
-                                System.out.print("[ " + steps[down] + " ]");
-                                down--;
-                            }
-                            else{
-                                if (j > 4 && j < cols - 5) {
-                                    System.out.print(" ");
-                                }
-                            }
+        while (true) {
+            for (int i = 0; i < board.getStepsForALap(); i++) {
+                switch (board.getBoardLevel()) {
+                    case LEARNING:
+                        switch (i) {
+                            case 0 -> steps[i] = "4";
+                            case 1 -> steps[i] = "3";
+                            case 2 -> steps[i] = "2";
+                            case 4 -> steps[i] = "1";
+                            default -> steps[i] = " ";
                         }
                         break;
-                    case 1:
-                        if (j == 2) {
-                            System.out.print("^");
-                        }
-                        else if (j == cols - 3) {
-                            System.out.print("|");
-                        }
-                        else{
-                            System.out.print(" ");
-                        }
-                        break;
-                    case 2:
-                        if (j == 2) {
-                            System.out.print("|");
-                        }
-                        else if (j == cols - 3) {
-                            System.out.print("v");
-                        }
-                        else{
-                            System.out.print(" ");
+                    case SECOND:
+                        switch (i) {
+                            case 0 -> steps[i] = "4";
+                            case 1 -> steps[i] = "3";
+                            case 3 -> steps[i] = "2";
+                            case 6 -> steps[i] = "1";
+                            default -> steps[i] = " ";
                         }
                         break;
                 }
             }
+            board.refreshInGamePlayers();
+            board.getInGamePlayers().forEach(player -> {
+                steps[player.getStep() % board.getStepsForALap()] = getPlayerColor(player);
+            });
+
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    switch (i % 3) {
+                        case 0:
+                            if (i == 0) {
+                                switch (j % 7) {
+                                    case 0:
+                                        System.out.print("[ " + steps[up] + " ]");
+                                        up++;
+                                        break;
+                                    case 5:
+                                        System.out.print("->");
+                                        break;
+                                }
+                            } else if (i == rows - 1) {
+                                switch (j % 7) {
+                                    case 0:
+                                        System.out.print("[ " + steps[down] + " ]");
+                                        down--;
+                                        break;
+                                    case 5:
+                                        System.out.print("<-");
+                                        break;
+                                }
+                            } else {
+                                if (j == cols - 5) {
+                                    System.out.print("[ " + steps[up] + " ]");
+                                    up++;
+                                } else if (j == 0) {
+                                    System.out.print("[ " + steps[down] + " ]");
+                                    down--;
+                                } else {
+                                    if (j > 4 && j < cols - 5) {
+                                        System.out.print(" ");
+                                    }
+                                }
+                            }
+                            break;
+                        case 1:
+                            if (j == 2) {
+                                System.out.print("^");
+                            } else if (j == cols - 3) {
+                                System.out.print("|");
+                            } else {
+                                System.out.print(" ");
+                            }
+                            break;
+                        case 2:
+                            if (j == 2) {
+                                System.out.print("|");
+                            } else if (j == cols - 3) {
+                                System.out.print("v");
+                            } else {
+                                System.out.print(" ");
+                            }
+                            break;
+                    }
+                }
+                System.out.println();
+            }
+
             System.out.println();
+            System.out.println("InGame players:");
+            board.refreshInGamePlayers();
+            board.getInGamePlayers().forEach(player -> {
+                System.out.print("- " + player.getUsername() + ": " + (player.getStep() % board.getStepsForALap()) + " step with color " + player.getColor().toString() + "\n");
+            });
+
+            System.out.println();
+            System.out.println("GaveUp players:");
+            board.getGaveUpPlayers().forEach(player -> {
+                System.out.print((player.getUsername() + 1) + " with color " + player.getColor().toString() + "\n");
+            });
+
+            System.out.println("");
         }
-
-        System.out.println();
-        System.out.println("InGame players:");
-        board.refreshInGamePlayers();
-        board.getInGamePlayers().forEach(player -> {
-            System.out.print(player.getPosition() + ") " + player.getUsername() + ": " + (player.getStep() % board.getStepsForALap()) + " step with color " + player.getColor().toString() + "\n");
-        });
-
-        System.out.println();
-        System.out.println("GaveUp players:");
-        board.getGaveUpPlayers().forEach(player -> {;
-            System.out.print((player.getUsername() + 1) + " with color " + player.getColor().toString() + "\n");
-        });
     }
 
     private String getPlayerColor(PlayerData player) {
