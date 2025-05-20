4968123703e6f1122a9ffb3dadc9cc6cce35adcb
Mz2305 <matteo.zappa23@gmail.com>
2025-05-20 14:36:32 +0200

    Add initial implementation of GuiTest with FXML integration

--- src/main/java/view/gui/GuiTest.java
+++ src/main/java/view/gui/GuiTest.java
@@ -1,13 +1,29 @@
 package view.gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class GuiTest extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GuiTest.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
+        launch();
+    }
+
+    /*public static void main(String[] args) {
         launch(args);
     }
 
@@ -19,6 +35,8 @@ public class GuiTest extends Application {
         stage.setScene(scene);
         stage.show();
     }
+
+     */
     /*
     Creo una schermata
     Runno un codice
