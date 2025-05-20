1faa306e1a19d0c50abd5cd0140246fa46c4e930
Mz2305 <matteo.zappa23@gmail.com>
2025-05-20 14:13:20 +0200

    Refactor GuiTest to use Group layout in Scene

--- src/main/java/view/gui/GuiTest.java
+++ src/main/java/view/gui/GuiTest.java
@@ -1,6 +1,7 @@
 package view.gui;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -11,16 +12,12 @@ public class GuiTest extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        try{
-            BorderPane root = new BorderPane();
-            Scene scene = new Scene(root, 400, 400);
-            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
     }
     /*
     Creo una schermata
