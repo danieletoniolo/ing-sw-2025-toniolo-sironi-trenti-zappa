1138eb75b0ee993b62dafb1d293c0cba9b2f4f5f
danieletoniolo <danieletoniolo@users.noreply.github.com>
2025-05-24 09:53:18 +0200

    Refactor Launcher class to simplify OS detection and add terminal launch functionality

---
+++ src/main/java/app/ApplicationServer.java
@@ -0,0 +1,20 @@
+package app;
+
+import utils.Launcher;
+
+/**
+ * ApplicationServer is a simple class that serves as the entry point for the server application.
+ * It uses the {@link Launcher} class to launch the server in a new terminal window based on the operating system.
+ * <p>
+ * This class is the one executed after JPackage is executed, so that the user can run the server without
+ * having to open a terminal and run the jar file manually.
+ */
+public class ApplicationServer {
+    public static void main(String[] args) {
+        try {
+            Launcher.launchTerminal("Server.jar");
+        } catch (Exception e) {
+            System.exit(1);
+        }
+    }
+}
--- src/main/java/utils/Launcher.java
+++ src/main/java/utils/Launcher.java
@@ -1,46 +1,17 @@
 package utils;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Launcher {
-
-    /**
-     * {@link Environment} static environment variable to define the environment in which the application is running.
-     */
-    private static Environment environment;
-
     /**
      * Static string to define the name of the application.
      * It is used to create the folder where the game files are stored.
      */
     private static final String name = "GalaxyTrucker";
-
-    /**
-     * Enumeration to define the environment in which the application is running.
-     */
-    private enum Environment {
-        WINDOWS,
-        MAC,
-        LINUX
-    }
-
-    /**
-     * Constructor to initialize the environment variable based on the operating system.
-     * It checks the system property "os.name" to determine the OS and sets the environment variable accordingly.
-     */
-    public Launcher() {
-        String os = System.getProperty("os.name").toLowerCase();
-        if (os.contains("win")) {
-            environment = Environment.WINDOWS;
-        } else if (os.contains("mac")) {
-            environment = Environment.MAC;
-        } else {
-            environment = Environment.LINUX;
-        }
-    }
 
     /**
      * Static method to get the data folder path based on the operating system.
@@ -50,11 +21,15 @@ public class Launcher {
      * @throws IOException if an I/O error occurs
      */
     public static Path getDataFolder() throws IOException {
-        Path path = switch (environment) {
-            case WINDOWS -> Paths.get(System.getenv("APPDATA"), name);
-            case MAC -> Paths.get(System.getProperty("user.home"), "Library", "Application Support", name);
-            case LINUX -> Paths.get(System.getProperty("user.home"), "." + name);
-        };
+        String os = System.getProperty("os.name").toLowerCase();
+        Path path;
+        if (os.contains("win")) {
+            path = Paths.get(System.getenv("APPDATA"), name);
+        } else if (os.contains("mac")) {
+            path = Paths.get(System.getProperty("user.home"), "Library", "Application Support", name);
+        } else {
+            path = Paths.get(System.getProperty("user.home"), "." + name);
+        }
 
         // Create the directory if it doesn't exist
         if (!Files.exists(path)) {
@@ -62,5 +37,51 @@ public class Launcher {
         }
 
         return path;
+    }
+
+    public static void launchTerminal(String jarToLaunch) throws IOException, URISyntaxException {
+        String os = System.getProperty("os.name").toLowerCase();
+
+        // Get the path absolute to the jar file
+        Path appDir = Paths.get(Launcher.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParent();
+        Path jarPath = appDir.resolve("Server.jar");
+
+        // TODO: This will be changed as soon as we have a integrated runtime
+        String javaCmd = "java -jar \"" + jarPath + "\"";
+
+        if (os.contains("win")) {
+            // start cmd /k "java -jar ..."
+            new ProcessBuilder("cmd", "/c", "start", "cmd", "/k", javaCmd).start();
+
+        } else if (os.contains("mac")) {
+            String appleScriptCommand = "tell application \"Terminal\" to do script \"" + javaCmd.replace("\"", "\\\"") + "\"";
+            new ProcessBuilder("osascript", "-e", appleScriptCommand).start();
+
+        } else if (os.contains("nux")) {
+            // Multiple terminal emulators are available on Linux, so we try to launch one of them
+            String[] terminals = {
+                    "x-terminal-emulator",
+                    "gnome-terminal",
+                    "konsole",
+                    "xfce4-terminal",
+                    "xterm"
+            };
+
+            boolean launched = false;
+            for (String term : terminals) {
+                try {
+                    new ProcessBuilder(term, "-e", "bash", "-c", javaCmd + "; read").start();
+                    launched = true;
+                    break;
+                } catch (IOException ignored) {}
+            }
+
+            if (!launched) {
+                Logger.getInstance().log(Logger.LogLevel.ERROR, "No terminal emulator found. Please run the command manually: " + javaCmd, false);
+            }
+
+        } else {
+            Logger.getInstance().log(Logger.LogLevel.ERROR, "Unknown operating system: " + os, false);
+        }
     }
 }
