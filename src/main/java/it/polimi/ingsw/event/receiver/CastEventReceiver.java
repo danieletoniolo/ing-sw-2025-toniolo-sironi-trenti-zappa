29fdc7d50c3c375e0a2e21ecdfd4fd8441b249ff
Vittorio Sironi <vittorio.sironi@icloud.com>
2025-05-28 16:22:04 +0200

    Refactor CastEventReceiver logging for debugging purpose

--- src/main/java/it/polimi/ingsw/event/receiver/CastEventReceiver.java
+++ src/main/java/it/polimi/ingsw/event/receiver/CastEventReceiver.java
@@ -73,7 +73,8 @@ public class CastEventReceiver<T extends Event> implements EventReceiver<T> {
                     listener.handle((T) data);
                     Logger.getInstance().log(Logger.LogLevel.INFO, "Event handled by listener: " + listener.getClass().getName(), false);
                 } catch (ClassCastException e) {
-                    throw new IllegalStateException("Cannot handle event of type " + data.getClass().getName() + " with listener of type " + listener.getClass().getName(), e);
+                    Logger.getInstance().log(Logger.LogLevel.ERROR, "Event " + data.getClass().getName() + " cannot be handled by listener: " + listener.getClass().getName(), false);
+                    throw IllegalArgumentException.class.cast(e);
                 }
             });
 
