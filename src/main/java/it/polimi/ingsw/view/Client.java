dca20123c9f99121912c8b7c5fd8c916c8f36716
Vittorio Sironi <vittorio.sironi@icloud.com>
2025-06-04 01:04:45 +0200

    Set default server address to localhost (127.0.0.1).
    Updated the default IP address for RMI and TCP connections to localhost, replacing the previous external IP. This ensures the client connects to a local server by default during development or testing.

--- src/main/java/it/polimi/ingsw/view/Client.java
+++ src/main/java/it/polimi/ingsw/view/Client.java
@@ -76,9 +76,9 @@ public class Client {
                 /*System.out.print("Enter IP: ");
                 String address = sc.nextLine();*/
                 //String address = "140.238.173.150";
-                //String address = "127.0.0.1";
+                String address = "127.0.0.1";
                 //String address = "192.168.67.224";
-                String address = "129.152.14.114";
+                //String address = "129.152.14.114";
                 Connection connection = new RMIConnection(address, 2551);
                 transceiver.connect(UUID.randomUUID(), connection);
 
@@ -98,9 +98,9 @@ public class Client {
                 /*System.out.print("Enter IP: ");
                 String address = sc.nextLine();*/
                 //String address = "140.238.173.150";
-                //String address = "127.0.0.1";
+                String address = "127.0.0.1";
                 //String address = "192.168.67.224";
-                String address = "129.152.14.114";
+                //String address = "129.152.14.114";
                 Connection connection = new TCPConnection(address, 2550);
                 transceiver.connect(UUID.randomUUID(), connection);
 
